2ab69e4d50bcd935ba56a934ce0e90c88e5e0dad
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-07-01 22:59:37 +0300

    Initial Commit

---
+++ Rock paper scissors/Rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_paper_scissorsApp.swift
+//  Rock paper scissors
+//
+//  Created by newbie on 01.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
