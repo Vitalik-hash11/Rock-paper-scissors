a57457a1932d1b7911832b37bfe5b3c285d72683
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-07-01 23:46:47 +0300

    implemented basic ui template

--- Rock paper scissors/ContentView.swift
+++ Rock paper scissors/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+    @State private var userСhoice = "📃"
+    
+    private let options = ["✂️", "📃", "🪨"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.indigo
+                .ignoresSafeArea()
+            VStack (spacing: 40) {
+                // score
+                Text("Score: \(score)")
+                    .font(.largeTitle)
+                    .foregroundColor(.primary)
+                // table
+                VStack {
+                    Text("🪨")
+                    Text(shouldWin ? "You win" : "You lose")
+                        .font(.title)
+                    Text(userСhoice)
+                }
+                    .font(.system(size: 50))
+                    .frame(maxWidth: .infinity)
+                    .background(.thinMaterial)
+                // buttons
+                HStack {
+                    ForEach(options, id: \.self) { option in
+                        Button {
+                            userСhoice = option
+                            shouldWin = Bool.random()
+                        } label: {
+                            Text(option)
+                                .padding()
+                                .background(.gray)
+                                .font(.system(size: 50))
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
