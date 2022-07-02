724f7764e84fc6316c8f930f008c6f15dfe7cef9
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-07-02 13:29:11 +0300

    implement logic for game

--- Rock paper scissors/ContentView.swift
+++ Rock paper scissors/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct ContentView: View {
     @State private var shouldWin = Bool.random()
     @State private var score = 0
-    @State private var userСhoice = "📃"
+    @State private var userСhoice = " "
+    @State private var computerСhoice = "✂️"
+    @State private var shouldDraw = Int.random(in: 0...5) == 5
+    @State private var gameCount = 0
+    @State private var showFinalAlert = false
     
     private let options = ["✂️", "📃", "🪨"]
     
@@ -23,9 +27,13 @@ struct ContentView: View {
                 Text("Score: \(score)")
                     .font(.largeTitle)
                     .foregroundColor(.primary)
-                // table
+                // game table
                 VStack {
-                    Text("🪨")
+                    Text(computerСhoice)
+                    shouldDraw ?
+                    Text("Draw")
+                        .font(.title)
+                    :
                     Text(shouldWin ? "You win" : "You lose")
                         .font(.title)
                     Text(userСhoice)
@@ -39,6 +47,8 @@ struct ContentView: View {
                         Button {
                             userСhoice = option
                             shouldWin = Bool.random()
+                            shouldDraw = Int.random(in: 0...5) == 5
+                            updateComputerChoice()
                         } label: {
                             Text(option)
                                 .padding()
@@ -48,6 +58,39 @@ struct ContentView: View {
                     }
                 }
             }
+        } .alert("Your final score: \(score)", isPresented: $showFinalAlert) {
+            Button("Continue") {
+                gameCount = 0
+            }
+            Button("Restart") {
+                score = 0
+                gameCount = 0
+            }
+        }
+    }
+    
+    private func updateComputerChoice() {
+        gameCount += 1
+        if gameCount == 5 {
+            showFinalAlert = true
+        }
+        switch userСhoice {
+        case "📃":
+            computerСhoice = shouldWin ? "🪨" : "✂️"
+        case "🪨":
+            computerСhoice = shouldWin ? "✂️" : "📃"
+        case "✂️":
+            computerСhoice = shouldWin ? "📃" : "🪨"
+        default:
+            fatalError("Unknown choice")
+        }
+        if shouldDraw {
+            computerСhoice = userСhoice
+        }
+        if computerСhoice != userСhoice && shouldWin {
+            score += 1
+        } else if computerСhoice != userСhoice && !shouldWin {
+            score -= 1
         }
     }
 }
